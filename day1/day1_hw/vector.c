3c5c5d20c8969a0f3fc12c35355eb36c33e65975
Ivan Krastev <krastev1892@gmail.com>
2017-11-04 17:11:27 +0200

    day1-hw-done

--- day1/day1_hw/vector.c
+++ day1/day1_hw/vector.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <math.h>
 #include "vector.h"
 
 void print_vec(struct vector v);
@@ -7,21 +5,21 @@ float scalar(struct vector v1, struct vector v2);
 float length(struct vector v1, struct vector v2);
 float scalar_product(struct vector v1, struct vector v2);
 
-int main(void){
+int main (void){
 
-	struct vector vec1,vec2;
-	vec1.x=1.0f;
-	vec1.y=2.0f;
-	vec1.z=3.0f;
+	struct vector vec1, vec2;
+	vec1.x = 1.0f;
+	vec1.y = 2.0f;
+	vec1.z = 3.0f;
 
-	vec2.x=10.0f;
-	vec2.y=20.0f;
-	vec2.z=30.0f;
+	vec2.x = 10.0f;
+	vec2.y = 20.0f;
+	vec2.z = 30.0f;
 
 	/*print_vec(vec);*/
 	/*printf("%f\n",scalar(vec1,vec2));*/
-    /*printf("%f\n",length(vec1,vec2));*/
-    printf("%f\n",scalar_product(vec1,vec2));
+	/*printf("%f\n",length(vec1,vec2));*/
+	printf("%f\n", scalar_product(vec1, vec2));
 
 	return 0;
 }
